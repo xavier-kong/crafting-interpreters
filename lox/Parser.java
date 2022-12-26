e7b4f080b90d7973dc253b56195482a60e708d86
xavier-kong <kongxianchengxavier@gmail.com>
2022-12-26 17:47:15 +0800

    ok i did not realize the learning curve would be this steep....

---
+++ lox/Parser.java
@@ -0,0 +1,30 @@
+package lox;
+
+import java.util.List;
+
+import static lox.TokenType.*;
+
+class Parser {
+  private final List<Token> tokens;
+  private int current = 0;
+
+  Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+  private Expr expression() {
+    return equality();
+  }
+
+  private Expr equality() {
+    Expr expr = comparison();
+
+    while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+}
